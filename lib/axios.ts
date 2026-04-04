3e83cd9e5c38f5f66262cde75d70af625c1fdc61
cindy vira safitri <cindyvira929@email.com>
2026-04-04 20:13:21 +0700

    connect to api laravel

---
+++ lib/axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+export default axiosInstance;
