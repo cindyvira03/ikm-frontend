a935beaf31470b692b48c68efc157e765c6a65f5
cindyvira03 <150648752+cindyvira03@users.noreply.github.com>
2026-06-29 01:41:29 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "api.ikmprobolinggo.com",
-        pathname: "/storage/**",
+        pathname: "/**",
       },
     ],
   }
